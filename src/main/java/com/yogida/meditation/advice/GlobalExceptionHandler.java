fa2dbe31b80380a7af5674a369b5722408cfeeb5
avazhenindev <avazhenin.dev@gmail.com>
2026-07-28 23:01:53 +0500

    Bucket not found details

--- src/main/java/com/yogida/meditation/advice/GlobalExceptionHandler.java
+++ src/main/java/com/yogida/meditation/advice/GlobalExceptionHandler.java
@@ -124,7 +124,8 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NoSuchBucketException.class)
     public ResponseEntity<Map<String, Object>> handleNoSuchBucketException(NoSuchBucketException ex, HttpServletRequest request) {
         log.error("Bucket not found on [{} {}]: {}", request.getMethod(), request.getRequestURI(), ex.getMessage(), ex);
-        return buildResponse(HttpStatus.NOT_FOUND, "Bucket not found", request);
+        log.error("Bucket not found details: {}", ex.awsErrorDetails().errorMessage());
+        return buildResponse(HttpStatus.NOT_FOUND, "Bucket not found ", request);
     }
 
     /**
